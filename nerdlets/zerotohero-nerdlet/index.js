d867597331b1c0c7492d76c27ef0776cdaf28cbc
Sean Callan <sean.callan@ecs-digital.co.uk>
2020-10-27 21:17:57 +0000

    Chapter 4 complete

--- nerdlets/zerotohero-nerdlet/index.js
+++ nerdlets/zerotohero-nerdlet/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Grid, GridItem, HeadingText } from 'nr1';
+import Z2HIcon from './icon.png';
 
 // https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
 
 export default class ZerotoheroNerdletNerdlet extends React.Component {
     render() {
-        return <h1>Hello, zerotohero-nerdlet Nerdlet!</h1>;
+        return <>
+            <Grid>
+                <GridItem columnSpan={1}><img src={Z2HIcon} alt="Zero to Hero" height="80"/></GridItem>
+                <GridItem columnSpan={11} >
+                    <HeadingText
+                        tagType={HeadingText.TAG_TYPE.H1}
+                        className="MainHeading"
+                    >
+                        Zero to Hero!
+                    </HeadingText>
+                </GridItem>
+            </Grid>
+            <Grid>
+                <GridItem columnSpan={6}>Graph</GridItem>
+                <GridItem columnSpan={6}>Graph</GridItem>
+            </Grid>
+            <Grid>
+                <GridItem columnSpan={6}>Graph</GridItem>
+                <GridItem columnSpan={6}>Graph</GridItem>
+            </Grid>    
+            <Grid>
+                <GridItem columnSpan={6}>Graph</GridItem>
+                <GridItem columnSpan={6}>Graph</GridItem>
+            </Grid>
+        </>;
     }
 }
