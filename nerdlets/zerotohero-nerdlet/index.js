bccc0c860eb2b717f035c404f525e0b79bf05999
Sean Callan <sean.callan@ecs-digital.co.uk>
2020-10-27 21:43:16 +0000

    Chapter 5 complete

--- nerdlets/zerotohero-nerdlet/index.js
+++ nerdlets/zerotohero-nerdlet/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Grid, GridItem, HeadingText } from 'nr1';
+import { Grid, GridItem, HeadingText, PieChart, LineChart, BillboardChart } from 'nr1';
 import Z2HIcon from './icon.png';
 
 // https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
 
 export default class ZerotoheroNerdletNerdlet extends React.Component {
     render() {
+        const accountId=1951995
+        const appName="My Application (Ruby)"
         return <>
             <Grid>
                 <GridItem columnSpan={1}><img src={Z2HIcon} alt="Zero to Hero" height="80"/></GridItem>
@@ -19,9 +21,33 @@ export default class ZerotoheroNerdletNerdlet extends React.Component {
                 </GridItem>
             </Grid>
             <Grid>
-                <GridItem columnSpan={6}>Graph</GridItem>
-                <GridItem columnSpan={6}>Graph</GridItem>
-            </Grid>
+                <GridItem columnSpan={2}>
+                    <HeadingText tagType={HeadingText.TAG_TYPE.H2}>
+                        {appName}
+                    </HeadingText>
+                    <BillboardChart
+                        accountId={accountId}
+                        query={`select count(*) as 'Transactions' FROM Transaction where appName like '${appName}' SINCE 1 week ago`}
+                        fullWidth
+                    />
+                </GridItem>
+                <GridItem columnSpan={5}>
+                    <PieChart
+                        accountId={accountId}
+                        query={`select count(*) as 'Transactions' FROM Transaction where appName like '${appName}' facet appName limit 100 SINCE 1 week ago`}
+                        fullWidth
+                        fullHeight
+                    />
+                </GridItem>
+                <GridItem columnSpan={5}>
+                    <LineChart
+                        accountId={accountId}
+                        query={`select count(*) as 'Transactions' FROM Transaction where appName like '${appName}' facet appName limit max timeseries SINCE 1 week ago`}
+                        fullWidth
+                        fullHeight
+                    />
+                </GridItem>
+            </Grid>  
             <Grid>
                 <GridItem columnSpan={6}>Graph</GridItem>
                 <GridItem columnSpan={6}>Graph</GridItem>
