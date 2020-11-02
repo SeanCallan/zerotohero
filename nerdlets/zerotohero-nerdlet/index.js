cf7a7670efb01f2fe25e0813ac8839d4343f7b30
SeanCallan <sean.callan@virginmedia.com.com>
2020-11-02 14:11:45 +0000

    ls

--- nerdlets/zerotohero-nerdlet/index.js
+++ nerdlets/zerotohero-nerdlet/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, GridItem, HeadingText, PieChart, LineChart, BillboardChart, Icon } from 'nr1';
+import { Grid, GridItem, HeadingText, PieChart, LineChart, BillboardChart, Icon, PlatformStateContext } from 'nr1';
 import Z2HIcon from './icon.png';
 
 // https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
@@ -25,51 +25,64 @@ export default class ZerotoheroNerdletNerdlet extends React.Component {
                 likeClause: "%Service"
             }
         ]
-        const rows = appConfig.map((row, index)=>{
-            console.log(`${index}: ${row.name} - ${row.likeClause}`)
-            return <Grid key={index} className="ChartRow">
-            <GridItem columnSpan={2}>
-                <HeadingText tagType={HeadingText.TAG_TYPE.H2}>
-                <Icon sizeType={Icon.SIZE_TYPE.LARGE} type={row.icon} /> {row.name}
-                </HeadingText>
-                <BillboardChart
-                    accountId={accountId}
-                    query={`select count(*) as 'Transactions' FROM Transaction where appName like '${row.likeClause}' SINCE 1 week ago`}
-                    fullWidth
-                />
-            </GridItem>
-            <GridItem columnSpan={5}>
-                <PieChart
-                    accountId={accountId}
-                    query={`select count(*) as 'Transactions' FROM Transaction where appName like '${row.likeClause}' facet appName limit 100 SINCE 1 week ago`}
-                    fullWidth
-                    fullHeight
-                />
-            </GridItem>
-            <GridItem columnSpan={5}>
-                <LineChart
-                    accountId={accountId}
-                    query={`select count(*) as 'Transactions' FROM Transaction where appName like '${row.likeClause}' facet appName limit max timeseries SINCE 1 week ago`}
-                    fullWidth
-                    fullHeight
-                />
-            </GridItem>
-        </Grid>
-        })
-        console.log("rows", rows)
-        return <>
-            <Grid>
-                <GridItem columnSpan={1} className="AppIcon"><img src={Z2HIcon} alt="Zero to Hero" height="80"/></GridItem>
-                <GridItem columnSpan={11} >
-                    <HeadingText
-                        tagType={HeadingText.TAG_TYPE.H1}
-                        className="MainHeading"
-                    >
-                        Zero to Hero!
-                    </HeadingText>
-                </GridItem>
-            </Grid>
-            {rows}
-        </>;
-    }
-}
+        return <PlatformStateContext.Consumer>
+            {(platformUrlState) => {
+                console.debug("platformUrlState",platformUrlState)
+                console.log("timeRange",platformUrlState.timeRange)
+                let sinceClause = ""
+                if(platformUrlState && platformUrlState.timeRange) {
+                    if(platformUrlState.timeRange.duration) {
+                        sinceClause = `since ${platformUrlState.timeRange.duration/1000/60} minutes ago`
+                    } else if(platformUrlState.timeRange.begin_time && platformUrlState.timeRange.end_time){
+                        sinceClause = `since ${platformUrlState.timeRange.begin_time} until ${platformUrlState.timeRange.end_time}`
+                    }
+                }
+                const rows = appConfig.map((row, index)=>{
+                    console.log(`${index}: ${row.name} - ${row.likeClause}`)
+                    return <Grid key={index} className="ChartRow">
+                    <GridItem columnSpan={2}>
+                        <HeadingText tagType={HeadingText.TAG_TYPE.H2}>
+                        <Icon sizeType={Icon.SIZE_TYPE.LARGE} type={row.icon} /> {row.name}
+                        </HeadingText>
+                        <BillboardChart
+                            accountId={accountId}
+                            query={`select count(*) as 'Transactions' FROM Transaction where appName like '${row.likeClause}' ${sinceClause}`}
+                            fullWidth
+                        />
+                    </GridItem>
+                    <GridItem columnSpan={5}>
+                        <PieChart
+                            accountId={accountId}
+                            query={`select count(*) as 'Transactions' FROM Transaction where appName like '${row.likeClause}' facet appName limit 100 ${sinceClause}`}
+                            fullWidth
+                            fullHeight
+                        />
+                    </GridItem>
+                    <GridItem columnSpan={5}>
+                        <LineChart
+                            accountId={accountId}
+                            query={`select count(*) as 'Transactions' FROM Transaction where appName like '${row.likeClause}' facet appName limit max timeseries ${sinceClause}`}
+                            fullWidth
+                            fullHeight
+                        />
+                    </GridItem>
+                </Grid>
+                })
+                console.log("rows", rows)
+                return <>
+                    <Grid>
+                        <GridItem columnSpan={1} className="AppIcon"><img src={Z2HIcon} alt="Zero to Hero" height="80"/></GridItem>
+                        <GridItem columnSpan={11} >
+                            <HeadingText
+                                tagType={HeadingText.TAG_TYPE.H1}
+                                className="MainHeading"
+                            >
+                                Zero to Hero!
+                            </HeadingText>
+                        </GridItem>
+                    </Grid>
+                    {rows}
+                </>
+            }}
+        </PlatformStateContext.Consumer>
+}}
