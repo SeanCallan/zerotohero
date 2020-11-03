6daf9bb981c856ba9ded4a60d99b3853c661ca9b
SeanCallan <sean.callan@virginmedia.com.com>
2020-11-03 15:53:45 +0000

    added comments to make it easier for myself to understand how it works

--- components/MultiChart/index.js
+++ components/MultiChart/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { Spinner, NerdGraphQuery, LineChart } from 'nr1';
 
 export default class MultiChart extends Component {
+    //specify properties that are required by the multichart component
     static propTypes = {
         accountId: PropTypes.number.isRequired,
         sinceClause: PropTypes.string
@@ -13,10 +14,13 @@ export default class MultiChart extends Component {
         this.state = { data:null }
     }
     
+    //loads data into chart and specifies auto-update interval
     async componentDidMount() {
         this.loadData()
+        this.autoRefresh = setInterval(() => this.loadData(), 1000*60*1)
     }
 
+    //ensures data is matches time picker and is current
     componentDidUpdate(prevProps) {
         if (prevProps.sinceClause!==this.props.sinceClause){
             this.loadData()
@@ -29,7 +33,7 @@ export default class MultiChart extends Component {
         const variables = {
             id: accountId
         }
-    
+        //nrql query to retrieve data on the number of transactions and page views from insights
         let query = `
             query($id: Int!) {
                 actor {
@@ -42,9 +46,8 @@ export default class MultiChart extends Component {
         `
         const q = NerdGraphQuery.query({ query: query, variables: variables });
         console.log(q)
-        let formattedData = []
         q.then(results => {
-
+            //function for formatting data so it can be added to the line chart
             const dataFormatter = (data, fieldName, id, name, color) =>{
                 return {
                     metadata: {
@@ -60,7 +63,7 @@ export default class MultiChart extends Component {
                     data: data.results.map((datapoint)=>{ return { x: datapoint.endTimeSeconds*1000, y:datapoint[fieldName] }})
                 }
             }
-        
+            //formatt and add queried data to line chart
             let formattedData=[]
             formattedData.push(dataFormatter(results.data.actor.account.server,'transactions','series1','Server App', '#a35ebf'))
             formattedData.push(dataFormatter(results.data.actor.account.browser,'pageViews','series2','Browser App', '#ff0000'))
